5a1085a669ed98f3fb80d2ff734b8e51ac70e37f
Greg Lyons <gregory.lyons512@gmail.com>
2022-05-02 16:23:05 -0500

    Set up Tailwind and Prettier.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        accent: "hsl(25, 97%, 53%)",
+        lightgray: "hsl(217, 12%, 63%)",
+        gray: "hsl(216, 12%, 54%)",
+        darkblue: "hsl(213, 19%, 18%)",
+        verydarkblue: "hsl(216, 12%, 8%)"
+      }
+    },
+  },
+  plugins: [],
+}
